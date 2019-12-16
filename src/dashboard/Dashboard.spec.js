22cd5b4cbd5e2b142fbbe8d72441e16cb7cac79a
Anthony Lopez <anthonybeconscious@gmail.com>
2019-12-16 14:58:50 -0600

    dashboard test successfull

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
 // Test away
+import React from "react"
+import { render } from "@testing-library/react"
+import Dashboard from "./Dashboard"
+
+test("Dashboard is rendered", ()=>{
+    expect(render(<Dashboard/>)).toMatchSnapshot();
+})
