4ff82dd7f6cd0ced1c0a86998d01f5daceb58215
Anthony Lopez <anthonybeconscious@gmail.com>
2019-12-16 15:08:13 -0600

    successfull test/Display renders correctly

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+import React from "react"
+import { render } from "@testing-library/react"
+import Display from "./Display"
+import "@testing-library/jest-dom/extend-expect"
+
+test("Display renders", ()=>{
+    expect(render(<Display/>)).toMatchSnapshot();
+})
